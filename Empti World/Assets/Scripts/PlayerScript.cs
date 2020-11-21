045ce83ea94a71fc3cfa472a03ba28cbacb11470
Markus Ruplitsch <markus.ruplitsch@gmail.com>
2020-11-21 12:27:58 +0100

    added player movement

---
+++ Empti World/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(10, 10);
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
+        GameObject camera = GameObject.Find("Main Camera");
+        camera.transform.Translate(movement);
+    }
+}
