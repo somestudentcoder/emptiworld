29f12ba7d0cbc7a714f60ee09ea25ce1aa289c54
Markus Ruplitsch <markus.ruplitsch@gmail.com>
2020-12-18 15:46:09 +0100

    added tooltips to inventory and displaying the current resources

--- Empti World/Assets/Scripts/InventoryScript.cs
+++ Empti World/Assets/Scripts/InventoryScript.cs
@@ -3,20 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class InventoryScript : MonoBehaviour
 {
     private bool showInventory = false;
     private GameObject inventoryUI;
 
-    // Text fields to display resource numbers
-    private Text cable_resource_text;
-    private Text microchip_resource_text;
-    private Text stone_resource_text;
-    private Text iron_resource_text;
-    private Text coal_resource_text;
-    private Text wood_resource_text;
-    private Text water_resource_text;
-    private Text crop_resource_text;
 
     public GameObject house;
     public GameObject oven;
@@ -24,25 +16,56 @@ public class InventoryScript : MonoBehaviour
     public GameObject mine;
     public GameObject sawmill;
 
-    // All resources a player can have
+    // Text fields to display resource numbers
     // Basic resources
-    public int cable = 0;
-    public int microchip = 0;
+    private Text stone_resource_text;
+    private Text iron_ore_resource_text;
+    private Text wood_resource_text;
+    private Text water_resource_text;
+    private Text crop_resource_text;
+
+    // Common resources
+    private Text coal_resource_text;
+    private Text iron_ingot_resource_text;
+    private Text bucket_resource_text;
+
+    // Technology resources
+
+    // Construction resources
+    private Text tin_ore_resource_text;
+    private Text copper_ore_resource_text;
+    private Text tin_ingot_resource_text;
+    private Text copper_ingot_resource_text;
+    private Text brass_ingot_resource_text;
+    private Text steam_engine_resource_text;
+    private Text engine_fuel_resource_text;
+
+    // Alchemy resources
+
+    // Resource variables
+    // Basic resources
     public int stone = 0;
-    public int iron = 0;
-    public int coal = 0;
+    public int iron_ore = 0;
     public int wood = 0;
     public int water = 0;
     public int crop = 0;
 
+
     // Common resources
+    public int coal = 0;
+    public int iron_ingot = 0;
     public int bucket = 0;
 
     // Technology resources
 
     // Construction resources
+    public int tin_ore = 0;
+    public int copper_ore = 0;
+    public int tin_ingot = 0;
+    public int copper_ingot = 0;
+    public int brass_ingot = 0;
     public int steam_engine = 0;
-    public int brass_pipe = 0;
+    public int engine_fuel = 0;
 
     // Alchemy resources
 
@@ -51,14 +74,30 @@ public class InventoryScript : MonoBehaviour
     void Start()
     {
         // Set all text fields
-        cable_resource_text = GameObject.Find("Cable Resource Text").GetComponent<Text>();
-        microchip_resource_text = GameObject.Find("Microchip Resource Text").GetComponent<Text>();
+        // Basic resources 
         stone_resource_text = GameObject.Find("Stone Resource Text").GetComponent<Text>();
-        iron_resource_text = GameObject.Find("Iron Resource Text").GetComponent<Text>();
-        coal_resource_text = GameObject.Find("Coal Resource Text").GetComponent<Text>();
+        iron_ore_resource_text = GameObject.Find("Iron Ore Resource Text").GetComponent<Text>();
         wood_resource_text = GameObject.Find("Wood Resource Text").GetComponent<Text>();
         water_resource_text = GameObject.Find("Water Resource Text").GetComponent<Text>();
         crop_resource_text = GameObject.Find("Crop Resource Text").GetComponent<Text>();
+
+        //Common resources
+        coal_resource_text = GameObject.Find("Coal Resource Text").GetComponent<Text>();
+        iron_ingot_resource_text = GameObject.Find("Iron Ingot Resource Text").GetComponent<Text>();
+        bucket_resource_text = GameObject.Find("Bucket Resource Text").GetComponent<Text>();
+
+        // Technology resources
+
+        // Construction resources
+        tin_ore_resource_text = GameObject.Find("Tin Ore Resource Text").GetComponent<Text>();
+        copper_ore_resource_text = GameObject.Find("Copper Ore Resource Text").GetComponent<Text>();
+        tin_ingot_resource_text = GameObject.Find("Tin Ingot Resource Text").GetComponent<Text>();
+        copper_ingot_resource_text = GameObject.Find("Copper Ingot Resource Text").GetComponent<Text>();
+        brass_ingot_resource_text = GameObject.Find("Brass Ingot Resource Text").GetComponent<Text>();
+        steam_engine_resource_text = GameObject.Find("Steam Engine Resource Text").GetComponent<Text>();
+        engine_fuel_resource_text = GameObject.Find("Engine Fuel Resource Text").GetComponent<Text>();
+
+        // Alchemy resources
 
         inventoryUI = GameObject.Find("Inventory");
         inventoryUI.gameObject.SetActive(false);
@@ -67,14 +106,36 @@ public class InventoryScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        cable_resource_text.text = "x" + cable;
-        microchip_resource_text.text = "x" + microchip;
+        // Update all text fields
+        // Basic resources 
         stone_resource_text.text = "x" + stone;
-        iron_resource_text.text = "x" + iron;
-        coal_resource_text.text = "x" + coal;
+        iron_ore_resource_text.text = "x" + iron_ore;
         wood_resource_text.text = "x" + wood;
+        
         water_resource_text.text = "x" + water;
         crop_resource_text.text = "x" + crop;
+        tin_ore_resource_text.text = "x" + tin_ore;
+        copper_ore_resource_text.text = "x" + copper_ore;
+
+        //Common resources
+        coal_resource_text.text = "x" + coal;
+        iron_ingot_resource_text.text = "x" + iron_ingot;
+        bucket_resource_text.text = "x" + bucket;
+
+        // Technology resources
+
+        // Construction resources
+        tin_ore_resource_text.text = "x" + tin_ore;
+        copper_ore_resource_text.text = "x" + copper_ore;
+        tin_ingot_resource_text.text = "x" + tin_ingot;
+        copper_ingot_resource_text.text = "x" + copper_ingot;
+        brass_ingot_resource_text.text = "x" + brass_ingot;
+        steam_engine_resource_text.text = "x" + steam_engine;
+        engine_fuel_resource_text.text = "x" + engine_fuel;
+
+        // Alchemy resources
+
+
 
         // Open/Close Inventory
         if (Input.GetKeyDown("e"))
@@ -100,12 +161,12 @@ public class InventoryScript : MonoBehaviour
     public void craftBucket()
     {
         // Required resources
-        const int required_iron = 4;
+        const int required_iron_ingot = 4;
 
         // Continue only if all requirements are met
-        if (iron >= required_iron)
+        if (iron_ingot >= required_iron_ingot)
         {
-            iron -= required_iron;
+            iron_ingot -= required_iron_ingot;
             bucket += 1;
         }
     }
@@ -128,15 +189,15 @@ public class InventoryScript : MonoBehaviour
     public void craftBasicHouse()
     {
         // Required resources
-        const int required_iron = 4;
+        const int required_iron_ingot = 4;
         const int required_stone = 8;
         const int required_wood = 16;
 
         // Continue only if all requirements are met
-        if (iron >= required_iron && stone >= required_stone && wood >= required_wood)
+        if (iron_ingot >= required_iron_ingot && stone >= required_stone && wood >= required_wood)
         {
 
-            iron -= required_iron;
+            iron_ingot -= required_iron_ingot;
             stone -= required_stone;
             wood -= required_wood;
 
@@ -147,13 +208,13 @@ public class InventoryScript : MonoBehaviour
     public void craftMine()
     {
         // Required resources
-        const int required_iron = 8;
+        const int required_iron_ingot = 8;
         const int required_stone = 8;
         const int required_wood = 8;
 
-        if (iron >= required_iron && stone >= required_stone && wood >= required_wood)
+        if (iron_ingot >= required_iron_ingot && stone >= required_stone && wood >= required_wood)
         {
-            iron -= required_iron;
+            iron_ingot -= required_iron_ingot;
             stone -= required_stone;
             wood -= required_wood;
 
@@ -164,13 +225,13 @@ public class InventoryScript : MonoBehaviour
     public void craftSawmill()
     {
         // Required resources
-        const int required_iron = 8;
-        const int required_stone = 0;
+        const int required_iron_ingot = 8;
+        const int required_stone = 8;
         const int required_wood = 8;
 
-        if (iron >= required_iron && stone >= required_stone && wood >= required_wood)
+        if (iron_ingot >= required_iron_ingot && stone >= required_stone && wood >= required_wood)
         {
-            iron -= required_iron;
+            iron_ingot -= required_iron_ingot;
             stone -= required_stone;
             wood -= required_wood;
 
@@ -182,17 +243,17 @@ public class InventoryScript : MonoBehaviour
     {
         switch (resource_name)
         {
-            case "Wood":
-                wood += amount;
-                break;
             case "Stone":
                 stone += amount;
                 break;
+            case "Iron Ore":
+                iron_ingot += amount;
+                break;
+            case "Wood":
+                wood += amount;
+                break;
             case "Coal":
                 coal += amount;
-                break;
-            case "Iron":
-                iron += amount;
                 break;
             case "Water":
                 water += amount;
@@ -200,11 +261,11 @@ public class InventoryScript : MonoBehaviour
             case "Crop":
                 crop += amount;
                 break;
-            case "MicroChip":
-                microchip += amount;
+            case "Tin Ore":
+                tin_ore += amount;
                 break;
-            case "Cable":
-                cable += amount;
+            case "Copper Ore":
+                copper_ore += amount;
                 break;
             default:
                 Debug.Log("This resource is not known: " + resource_name);
---
+++ Empti World/Assets/Scripts/ToolTipScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ToolTipScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject toolTip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toolTip.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.SetAsLastSibling();
+        toolTip.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        toolTip.SetActive(false);
+    }
+}
