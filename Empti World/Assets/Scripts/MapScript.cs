41ea9ed6e3d0637c6d6403c7f8ddf078bc75ff0a
Theresa <theresa.dietinger@gmail.com>
2021-02-07 20:10:25 +0100

    create big map

---
+++ Empti World/Assets/Scripts/MapScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScript : MonoBehaviour
+{
+
+    private GameObject map;
+    private GameObject mapBorder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = GameObject.Find("Map");
+        mapBorder = GameObject.Find("MapBorder");
+        map.gameObject.SetActive(false);
+        mapBorder.gameObject.SetActive(false);
+    }
+
+    public void OpenMap()
+    {
+        map.gameObject.SetActive(true);
+        mapBorder.gameObject.SetActive(true);
+    }
+
+    public void CloseMap()
+    {
+        map.gameObject.SetActive(false);
+        mapBorder.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
